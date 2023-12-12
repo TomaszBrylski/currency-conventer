fa84e23536d27d067a2ae8b7703df1cecbe740c5
Tomasz Brylski <brylskitomasz@gmail.com>
2023-12-13 00:16:10 +0100

    Initioal commit

---
+++ js/script.js
@@ -0,0 +1,45 @@
+{
+  const calculateResoult = (amount, currency) => {
+    const rateEUR = 4.3125;
+    const rateUSD = 3.9958;
+    const rateGBP = 5.0337;
+
+    switch (currency) {
+      case "EUR":
+        return amount / rateEUR;
+
+      case "USD":
+        return amount / rateUSD;
+
+      case "GBP":
+        return amount / rateGBP;
+    }
+  };
+
+  const updateResoultText = (amount, result, currency) => {
+    const resultElement = document.querySelector(".js-result");
+    resultElement.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency}<strong>`;
+  };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const amountElement = document.querySelector(".js-amount");
+    const currencyElement = document.querySelector(".js-currency");
+
+    const amount = +amountElement.value;
+    const currency = currencyElement.value;
+
+    const result = calculateResoult(amount, currency);
+
+    updateResoultText(amount, result, currency);
+  };
+
+  const init = () => {
+    const formElement = document.querySelector(".js-form");
+
+    formElement.addEventListener("submit", onFormSubmit);
+  };
+
+  init();
+}
